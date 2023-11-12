775b9147b76ab6d20bef35067cdd5c77c84f01f5
Robert Petersson <rpetersson@gmail.com>
2023-11-12 18:19:37 +0800

    update

---
+++ components/addForm.tsx
@@ -0,0 +1,45 @@
+"use client"
+import { useFormState } from "react-dom";
+import { useFormStatus } from "react-dom";
+import { Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { addRecord } from "@/app/page";
+import { init } from "next/dist/compiled/webpack/webpack";
+
+const prisma = new PrismaClient();
+
+
+
+function SubmitButton() {
+  const { pending } = useFormStatus()
+  return (
+    <button type="submit" aria-disabled={pending}>
+      Add
+    </button>
+  );    
+}
+
+export function AddForm() {
+  const [state, formAction] = useFormState(addRecord, null)
+  return (
+    <form action={formAction}>
+      <label htmlFor="todo">Enter Task</label>
+      <ul id="list">
+        <li>
+          <input
+            type="text"
+            placeholder="What do you need to do?"
+            className="text-black"
+            id="todo"
+            name="todo"
+            required
+          ></input>
+        </li>
+        <li>
+          <SubmitButton />
+        </li>
+      </ul>
+    </form>
+  );
+}
---
+++ prisma/migrations/20231112062449_init/migration.sql
@@ -0,0 +1,5 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "todoTask" TEXT NOT NULL
+);
