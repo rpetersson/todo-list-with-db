9f5c0fd5c5d2f8939b568042e5d2b48dabcfea17
Robert Petersson <rpetersson@gmail.com>
2023-11-22 12:00:57 +0800

    update

--- app/components/dbclient.tsx
+++ app/components/dbclient.tsx
@@ -2,9 +2,8 @@
 import { TableClient, AzureSASCredential } from "@azure/data-tables";
 
 const account = "todolistwithdb";
-const sas: string | undefined = process.env.SAS_TOKEN ?? "default";
+const sas = process.env.SAS_TOKEN as string;
 export const dbClient = new TableClient(
-  `https://${account}.table.core.windows.net`,
-  account,
+  `https://${account}.table.core.windows.net`, account,
   new AzureSASCredential(sas)
 );
